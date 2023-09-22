5508f342c6d6da75296430f68f9b92daf00c0b73
nbozsi <nbozsi4@gmail.com>
2023-09-22 12:01:01 +0200

    rename force

--- main.py
+++ main.py
@@ -27,22 +27,22 @@ def try_options(image_filename):
 @click.option('-c', "--count", default=2, help="Number of reshreds", show_default=True)
 @click.option('-s', "--stripes", default=200, help="Number of the stripes", show_default=True)
 @click.option('-o', "--output", default=None, help="Name of the output file")
-@click.option('-f', "--force", is_flag=True, default=False, help="Forces your args, no help finding files/rounding count etc..")
-def hello(filename, count, stripes, output, force):
+@click.option('-w', is_flag=True, default=False, help="No help finding files/rounding count and no warnings etc..")
+def hello(filename, count, stripes, output, w):
 
-    if os.path.exists(filename) or force:
+    if os.path.exists(filename) or w:
         input_image = Image.open(filename)
     else:
         print("This file does not exists.")
         input_image, filename = try_options(filename)
 
-    if count % 2 != 0 and not force:
+    if count % 2 != 0 and not w:
         print("Count is odd, so the output image will be stretched.")
         ok = input(f"Round it to {count+1}? (Y/n) ").lower()
         if ok == "" or ok == "y":
             count += 1
 
-    if stripes % count != 0 and not force:
+    if stripes % count != 0 and not w:
         print("Number of stripes is not divisibel by count, so the result images will slightly differ in width.")
     output_image = shredder(input_image, count, stripes)
     if output:
